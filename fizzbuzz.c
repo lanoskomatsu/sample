f2b18ae23c6f0cf3895bcc8238acf86504ac5805
Lanoskomatsu <komatsu@lanos.co.jp>
2014-10-29 18:14:35 +0900

    fizzbuzz

---
+++ fizzbuzz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(){
+    int i;
+
+    for(i=0; i<=30; i++){
+        if((i!=0) && (i%15 == 0)){
+            printf("fizzbuzz\n");
+        }
+        else if((i!=0) && (i%5 == 0)){
+            printf("buzz\n");
+        }
+        else if((i!=0) && (i%3 == 0)){
+            printf("fizz\n");
+        }
+        else{
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
